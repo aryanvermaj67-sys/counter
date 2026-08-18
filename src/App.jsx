03361b8c75eda12f7b0a13194e0c1c2817ed8b8f
Aryan <aryanvermaj67@gmail.com>
2026-08-18 21:14:38 +0530

    Add counter application files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>React Counter Application</h1>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+
+      <button onClick={() => setCount(count + 5)}>
+        Increase by 5
+      </button>
+    </div>
+  );
+}
+
+export default App;
